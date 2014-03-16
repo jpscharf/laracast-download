c908c0928553ea86be69abeaf0c4689796262de7
Keith Welch <keith@paralogiktech.com>
2014-03-16 14:58:23 -0400

    Download works using curl from command-line

--- laracast-download.php
+++ laracast-download.php
@@ -23,11 +23,6 @@ curl_setopt($ch, CURLOPT_COOKIEJAR, LD_COOKIEJAR);
 curl_setopt($ch, CURLOPT_COOKIEFILE, LD_COOKIEJAR);
 #curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 
-$casts = grab_all();
-if ($casts === false) die('Failed to grab all casts'.PHP_EOL);
-print_r($casts);
-die;
-
 $authed = false;
 if (file_exists(LD_COOKIEJAR)) {
   p('Checking if our previous session is still active ... ');
@@ -42,6 +37,33 @@ if (!$authed) {
 }
 
 p('Grab list from /all');
+$casts = grab_all();
+if ($casts === false) die('Failed to grab all casts'.PHP_EOL);
+
+$padding = 4;
+$lesson_ids = array();
+foreach ($casts as $cast) {
+  $lesson_id = get_lesson_id($cast);
+  if ($lesson_id === false) {
+    p('Failed to get lesson id for cast=' . $cast);
+    continue;
+  }
+  $out_file = $directory . '/' . str_pad($lesson_id, $padding, '0', STR_PAD_LEFT) . '_' . $cast . '.mp4';
+  if (file_exists($out_file)) {
+    p('File exists for out_file=' . $out_file . ', skipping');
+    continue;
+  }
+  $url = get_download_url($lesson_id);
+  if ($url === false) {
+    p('Could not get download url for lession_id=' . $lession_id);
+  }
+  p('Downloading ' . $url . ' to ' . $out_file);
+  $result = download_from_vimeo($out_file, $url);
+  if ($result===false) {
+    p('Failed to get url=' . $url);
+  }
+  p('Success, got lesson_id=' . $lesson_id . ', cast=' . $cast);
+}
 
 #functions
 /*
@@ -150,6 +172,71 @@ function grab_all() {
     }
   }
   return false;
+}
+/*
+ * Get download url from /lessons/<cast>
+ */
+function get_lesson_id($cast) {
+  global $ch;
+  p('Calling /lessons/' . $cast . ' ... ', 1);
+  curl_setopt($ch, CURLOPT_URL, 'https://laracasts.com/lessons/' . $cast);
+  $response=curl_exec($ch);
+  if ($response===false) {
+    curl_close($ch);
+    p('failed');
+    return false;
+  }
+  p('success');
+  dump_response_to_file($response);
+  if (preg_match('!^HTTP/1.1 200 OK!', $response)) {
+    p('Found 200 OK, checking cast links');
+    $matches=array();
+    if (preg_match('!/downloads/([0-9]+)!', $response, $matches)) {
+      $lesson_id = $matches[1];
+      p('Lesson ID for cast=' . $cast .' is ' . $lesson_id);
+      return $lesson_id;
+    }
+  }
+  return false;
+}
+/*
+ * Get the download URL
+ */
+function get_download_url($lesson_id) {
+  global $ch;
+  p('Calling /downloads/' . $lesson_id . ' ... ', 1);
+  curl_setopt($ch, CURLOPT_URL, 'https://laracasts.com/downloads/' . $lesson_id);
+  $response=curl_exec($ch);
+  if ($response===false) {
+    curl_close($ch);
+    p('failed');
+    return false;
+  }
+  p('success');
+  dump_response_to_file($response);
+  if (preg_match('!^HTTP/1.1 302 Found!', $response)) {
+    p('Found 302 Found, checking for Location');
+    $matches=array();
+    if (preg_match('!Location: (.*)!', $response, $matches)) {
+      $download_url = $matches[1];
+      p('Lesson ID for id=' . $lesson_id .' url is ' . $download_url);
+      return trim($download_url);
+    }
+  }
+  return false;
+}
+/*
+ * Download from Vimeo after some redirects
+ * Using command line curl because following
+ * redirects in php is usually disabled by
+ * safe_mode/open_basedr
+ */
+function download_from_vimeo($out_file, $url) {
+  $cmd="/usr/bin/curl -L -o $out_file -A 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36' '$url'";
+  p('Executing command ' . $cmd);
+  passthru($cmd);
+  if (file_exists($out_file)) return true;
+  else return false;
 }
 /*
  * Outputs message based on debug setting
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-### Laracast download script
+## Laracast download script
 
 This script will download all the laracasts videos into a
 folder you specify in the config file.
@@ -8,7 +8,7 @@ It still requires an active subscription account!
 Yes it is flat PHP, I am a dinosaur. Why do you think
 I got a Laracast subscription?
 
-## Usage
+### Usage
 
 Copy the cofig.example.php to config.php, set you options
 and account credintials and output directory, then run the
@@ -16,16 +16,32 @@ script.
 
 It will attempt to skip movies it already has gotten.
 
-## Requirements
+### Requirements
 
 PHP with curl and command line wget
 
-## TODO
+### TODO
 
 - ~~Authenicate with Laracast~~
 - ~~Get movie lists~~
-- Get movie download link
-- Download
+- ~~Get movie download link~~
+- ~~Download~~
 - Download only new videos
 - Add tag options
 
+### Warnings
+
+**Use at your own risk**. Jeffery Way has done an amazing job in
+all his videos and deserves kudos for his hard work helping
+promot Laravel.
+
+He does not endorse this tool in anyway.
+
+I made this tool primarly so that it could download the casts
+and put them on my phone. 
+
+I have a 16 month old (as of project creation date) and do not
+get a lot of time to sit in front of a traditional screen.
+
+As a new member I came in and there were 132 videos at the time.
+I got stick of downloading them all manually.
